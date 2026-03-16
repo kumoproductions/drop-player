6ae17fccb7cac296a89788c7b773f27d0513ca76
Tomoya Eguchi <mail@cumulo.works>
2026-03-16 22:44:27 +0900

    chore: disable sourcemap + minify

--- vite.config.ts
+++ vite.config.ts
@@ -49,8 +49,8 @@ export default defineConfig(({ command }) => {
               },
             },
             cssCodeSplit: false,
-            sourcemap: true,
-            minify: false,
+            sourcemap: false,
+            minify: true,
           },
         }
       : {}),
