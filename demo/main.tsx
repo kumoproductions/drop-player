e7afc10f3a6fe10d9deb12e24e063d1f3df59c43
Tomoya Eguchi <mail@cumulo.works>
2026-03-21 07:00:19 +0900

    feat: added vercel analytics

--- demo/main.tsx
+++ demo/main.tsx
@@ -1,7 +1,10 @@
+import { inject } from '@vercel/analytics';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
+
+inject();
 
 // biome-ignore lint/style/noNonNullAssertion: root element guaranteed in index.html
 createRoot(document.getElementById('root')!).render(
